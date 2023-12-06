8868f2fab1493a2350727f495fc9face57fcbb53
Carson-Wasmuth <148169140+Carson-Wasmuth@users.noreply.github.com>
2023-12-06 11:40:05 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,3 +11,21 @@ document.getElementById('alertButton').onmouseover = () => {
 document.getElementById('alertButton').onmouseleave = () => {
     document.getElementById('alertButton').innerText = 'Click Me!';
 };
+
+let count = 1;
+
+document.getElementById('btn-counter').onclick = () => {
+    count = count + 1;
+    document.getElementById('txt-counter').innerHTML = 'Number: ' + count;
+
+
+    if (count % 2 === 0) {
+        
+        document.getElementById('txt-counter').classList.add('even');
+        document.getElementById('txt-counter').classList.remove('odd');
+    } else {
+        
+        document.getElementById('txt-counter').classList.add('odd');
+        document.getElementById('txt-counter').classList.remove('even');
+    }
+};
