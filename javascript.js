ae0a6fbcb9bb87e22d43dbcb495cdc15021c8c95
Carson-Wasmuth <148169140+Carson-Wasmuth@users.noreply.github.com>
2023-12-06 11:30:08 -0700

    changes

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Hello world!');
+});
+
+document.getElementById('id').onmouseover = () => {
+    document.getElementById('id').innerText = 'Please!';
+};
+
+document.getElementById('id').onmouseleave = () => {
+    document.getElementById('id').innerText = 'Click Me!';
+};
